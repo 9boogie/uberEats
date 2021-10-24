d2fe975863667722432cec007a36cfedcb7a9b26
9boogie <jzero.youngs89@gmail.com>
2021-10-23 23:15:53 -0700

    Add restaurants

--- screens/Home.js
+++ screens/Home.js
@@ -1,17 +1,23 @@
-import React from "react";
-import { View, Text, SafeAreaView } from "react-native";
+import React, { useState } from "react";
+import { View, Text, SafeAreaView, ScrollView } from "react-native";
 import HeaderTabs from "../components/HeaderTabs";
 import SearchBar from "../components/SearchBar";
 import Categories from "../components/Categories";
+import RestaurantItems from "../components/RestaurantItems";
 
 export default function Home() {
+  const [restaurantData, setRestaurantData] = useState([]);
+
   return (
     <SafeAreaView style={{ backgroundColor: "#eee", flex: 1 }}>
       <View style={{ backgroundColor: "white", padding: 15 }}>
         <HeaderTabs />
         <SearchBar />
       </View>
-      <Categories />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <Categories />
+        <RestaurantItems restaurantData={restaurantData} />
+      </ScrollView>
     </SafeAreaView>
   );
 }
